c21d94962676e4aaf6a44542904c6c42516def05
Raul Ospina <raminka13@hotmail.com>
2024-11-18 19:25:14 -0500

    New: landing draft components

---
+++ src/data/navbarLinks.js
@@ -0,0 +1,22 @@
+export const navbarLinks = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "Private Charter",
+    link: "#charter",
+  },
+  {
+    name: "Aircraft Management",
+    link: "#aircraft-management",
+  },
+  {
+    name: "About Us",
+    link: "#about-us",
+  },
+  {
+    name: "Contact Us",
+    link: "#contact-us",
+  },
+];
