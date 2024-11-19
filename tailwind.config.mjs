ca8d0d59b86d930d4a321dff71330e7d3b34d4c2
Raul Ospina <raminka13@hotmail.com>
2024-11-18 19:24:42 -0500

    Update: add fonts, icons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Public Sans Variable", "sans-serif"],
+        sans2: ["Cabin Variable", "sans-serif"],
+        parag: ["Montserrat Variable", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
